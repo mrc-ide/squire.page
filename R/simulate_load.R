86bb709c94f1674d16865c82a89a4b69a4cae840
GBarnsley <gregbarnsley@hotmail.co.uk>
2022-01-22 17:53:22 +0000

    Add option for wider confidence interval in load simulation function

--- R/simulate_load.R
+++ R/simulate_load.R
@@ -5,7 +5,8 @@
 #' @param group_by A vector of strings of the variables to group the summary by,
 #' e.g. Iso3c code, age_group or date.
 #' @param quantileSamples Only used if dataset it not grouped by country. The
-#' number of combinations to randomly draw. Defaults to 2000.
+#' number of combinations to randomly draw. Defaults to 2000. If NULL creates
+#' a wider interval based on ordering samples.
 #' @param exclude_iso3cs A vector of iso3cs to exclude from the summaries,
 #' default = NULL.
 #' @export
@@ -118,52 +119,73 @@ loadCounterfactualData <- function(counterfactuals, group_by, quantileSamples = 
   counterfactual_data <- dplyr::group_by_at(counterfactual_data, c(group_by,"counterfactual"))
   #to find median/quantiles
   if(!normalMedian){
-    #else we randomly combine replicates and find their median/quantile over that
-    noReplicates <- length(unique(counterfactual_data$replicate))
-    uniqueCountries <- sort(unique(counterfactual_data$iso3c))
-    noCountries <- length(uniqueCountries)
-    #the median/quantiles of that
-    counterfactual_data <- counterfactual_data %>% #remove previous groupings:
-      dplyr::mutate(countryNumber =
-                      sapply(.data$iso3c,
-                             function(x){
-                               which(uniqueCountries == x)
-                             },
-                             USE.NAMES = F)
+    if(!is.null(quantileSamples)){
+      #else we randomly combine replicates and find their median/quantile over that
+      noReplicates <- length(unique(counterfactual_data$replicate))
+      uniqueCountries <- sort(unique(counterfactual_data$iso3c))
+      noCountries <- length(uniqueCountries)
+      #the median/quantiles of that
+      counterfactual_data <- counterfactual_data %>% #remove previous groupings:
+        dplyr::mutate(countryNumber =
+                        sapply(.data$iso3c,
+                               function(x){
+                                 which(uniqueCountries == x)
+                               },
+                               USE.NAMES = F)
+        )
+      #generate the random draws
+      #data set to hold results
+      #use group meta data to get output length
+      meta_data <- dplyr::group_data(counterfactual_data)
+      sample_length <- nrow(meta_data)
+      sample_names <- c(group_by, "counterfactual",
+                        "deaths", "infections",
+                        "baseline_deaths", "baseline_infections",
+                        "averted_deaths", "averted_infections"
       )
-    #generate the random draws
-    #data set to hold results
-    #use group meta data to get output length
-    meta_data <- dplyr::group_data(counterfactual_data)
-    sample_length <- nrow(meta_data)
-    sample_names <- c(group_by, "counterfactual",
-                      "deaths", "infections",
-                      "baseline_deaths", "baseline_infections",
-                      "averted_deaths", "averted_infections"
-    )
-    samples <- counterfactual_data[1,] %>%
-      dplyr::select(dplyr::all_of(sample_names))
+      samples <- counterfactual_data[1,] %>%
+        dplyr::select(dplyr::all_of(sample_names))
 
-    samples[1:(quantileSamples*sample_length),] <- NA
+      samples[1:(quantileSamples*sample_length),] <- NA
 
-    for(i in 1:quantileSamples){
-      #get random draw
-      replicates <- sample.int(noReplicates, size = noCountries, replace = T)
+      for(i in 1:quantileSamples){
+        #get random draw
+        replicates <- sample.int(noReplicates, size = noCountries, replace = T)
 
-      sum_data <- suppressMessages(counterfactual_data %>% #reduce to replicates used:
-        dplyr::filter(
-          .data$replicate == replicates[.data$countryNumber]
-        ) %>% #sum over the given groupings
+        sum_data <- suppressMessages(counterfactual_data %>% #reduce to replicates used:
+                                       dplyr::filter(
+                                         .data$replicate == replicates[.data$countryNumber]
+                                       ) %>% #sum over the given groupings
+                                       dplyr::summarise(
+                                         dplyr::across(
+                                           c(.data$deaths, .data$infections,
+                                             .data$baseline_deaths, .data$baseline_infections,
+                                             .data$averted_deaths, .data$averted_infections),
+                                           ~sum(.x, na.rm = TRUE)
+                                         )
+                                       ))
+        #add to samples
+        samples[seq(min(sample_length,nrow(sum_data))) + sample_length*(i-1),] <- sum_data
+      }
+    } else {
+      #otherwise we calculate a wider quantile
+      #reorder replicates
+      samples <- counterfactual_data %>% dplyr::arrange(.data$iso3c, .data$averted_deaths) %>%
+        dplyr::mutate(replicate = (seq_along(.data$iso3c) - 1) %% length(unique(.data$replicate)))
+
+      #group by groupings and other replicates
+      samples <- dplyr::group_by_at(samples,
+                                    c(group_by, "counterfactual", "replicate")) %>%
+        #sum across replicates
         dplyr::summarise(
           dplyr::across(
             c(.data$deaths, .data$infections,
               .data$baseline_deaths, .data$baseline_infections,
               .data$averted_deaths, .data$averted_infections),
             ~sum(.x, na.rm = TRUE)
-          )
-        ))
-      #add to samples
-      samples[seq(min(sample_length,nrow(sum_data))) + sample_length*(i-1),] <- sum_data
+          ),
+          .groups = "drop_last"
+        )
     }
     #re-add grouping
     counterfactual_data <- samples %>% #remove missing data
