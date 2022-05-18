05f47f403f4151ae749dc152b17aeb48e155ee12
GBarnsley <gregbarnsley@hotmail.co.uk>
2022-05-18 12:31:07 +0100

    Add more accurate model runs for the end and ability to change rt upper bound limits

--- R/particle_fit.R
+++ R/particle_fit.R
@@ -7,7 +7,7 @@
 #'
 #' This function is progressr enabled, so progressr::handlers(global = TRUE)
 #' can be used to view progress through the samples.
-#' 
+#'
 #' @param data A data frame of deaths occuring over a given time frame. Given in the
 #' format: deaths(integer), date_start(date), date_end(date). Must have at least
 #' one death period in each set of Rt trend changes (i.e. a 14 day period by
@@ -25,6 +25,8 @@
 #' @param proposal_width What proportion of the previous Rt trend to explore, default = 50%.
 #' @param n_particles How many particles to explore uniformly across that range, default = 7.
 #' @param k Control the dispersion on the negative binomial likelihood, default = 2.
+#' @param rt_upper_bound_max Largest value the upper bound of rt values explored can take, avoids estimate absurd Rt values, default = 20.
+#' @param rt_upper_bound_min Lowest value the upper bound of rt values explored can take, avoids Rt values getting stuck below 1, default = 5.
 #'
 #' @return An object of type rt_optimised, (model type).
 #'
@@ -37,7 +39,9 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
                          initial_infections = 100,
                          proposal_width = 0.5,
                          n_particles = 14,
-                         k = 2) {
+                         k = 2,
+                         rt_upper_bound_max = 20,
+                         rt_upper_bound_min = 5) {
   ##Initial Checks and Housekeeping
 
   #checks
@@ -95,23 +99,30 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
 
   #Run the Fitting Process
   if(parallel){
-    #set seed = NULL to suppress warnings due to squiress (unused) random initial infections
+    #set seed = NULL to suppress warnings due to squires (unused) random initial infections
     map_func <- function(.x, .f, ...) {
       furrr::future_map(.x, .f, ..., .options = furrr::furrr_options(seed = NULL))
     }
   } else {
+    #otherwise use purrr for easier debugging
     map_func <- purrr::map
   }
 
+  #setup progressr for user controlled feedback
   p <- progressr::progressor(steps = length(distribution), enable = TRUE)
+
+  #run the optimisation loop
   particle_output <- map_func(
+    #add distribution samples and parameters together
     purrr::map(distribution, ~append(.x, parameters)),
     function(parameters, data, initial_r, initial_infections, proposal_width, n_particles, k, squire_model){
+      #Determine the deaths used to fit each Rt
       temp <-  get_time_series(squire_model, parameters, data, rt_spacing)
       rt_df <- temp$rt_df
       split_data <- temp$split_data
       rm(temp)
 
+      ##series of model specific functions are generated
       #generate function that just returns model output
       model <- generate_model_function(squire_model, parameters)
       #generate function that returns the deaths from this output
@@ -121,19 +132,17 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
         this_data <- split_data[[rt_index]]
         #get deaths for entire rt period until end of death period, this way index in this_data is the correct position in this time series
         cumulative_deaths <- deaths_function(Rt, rt_df$rt_change_t[rt_index], rt_df$death_end_t[rt_index], initial_state)
-        #calculate likelihood for each timeperiod then sum
-        squire:::ll_nbinom(
-          data = this_data$deaths,
-          model = cumulative_deaths[this_data$index_end] - cumulative_deaths[this_data$index_start],
-          phi = 1,
-          k = k,
-          exp_noise = Inf #no noise
-        ) %>%
-          sum()
+        deaths <- cumulative_deaths[this_data$index_end] - cumulative_deaths[this_data$index_start]
+        #call generic likelihood function
+        ll_negative_binomial(deaths, this_data$deaths, k)
       }
       #function to get the initial state value from a model
       get_initial_state <- function(Rt, rt_index, initial_state){
-        model_output <- model(Rt, rt_df$rt_change_t[rt_index], rt_df$initial_target[rt_index], initial_state)
+        #generate odin output from current Rt trend time to when the next Rt trend begins
+        model_output <- model(Rt, rt_df$rt_change_t[rt_index], rt_df$initial_target[rt_index], initial_state) %>%
+          #only keep the final value
+          utils::tail(1)
+        #convert this output into initial state values
         update_initial_state(initial_state, model_output)
       }
       #R0 specific likelihood function
@@ -151,47 +160,59 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
                      assign_infections(initial_state, initial_infections)
         )
       }
-      #calculate R0 + initial number of infections, just optim for now
-      res <- particle_optimise_R0(initial_r, initial_infections, n_particles, proposal_width, R0_likelihood)
+      #calculate R0 + initial number of infections
+      res <- particle_optimise_R0(initial_r, initial_infections, n_particles, proposal_width, R0_likelihood, rt_upper_bound_max, rt_upper_bound_min)
       Rt_trend <- res[1]
       initial_infections <- res[2]
       names(Rt_trend) <- "R0"
+      #calculate the model state when the next rt trend starts
       initial_state <- R0_initial_state(res[1], res[2])
 
       #calculate each Rt trend onwards
       rt_trend <- purrr::reduce(
+        #skip the first index as that is R0
         seq_along(rt_df$rt_change_t)[-1],
         function(state_trend, rt_index){
+          #using state_trend$initial state, i.e. the state for the last Rt trend
+          #we search over the range Rt values based on the previous value
+          #(state_trend$rt_trend) and pick the highest likelihood
           res <- particle_optimise_Rt(rt = utils::tail(state_trend$Rt_trend, 1),
                                state_trend$initial_state, rt_index, n_particles,
-                               proposal_width, likelihood)
+                               proposal_width, likelihood, rt_upper_bound_max, rt_upper_bound_min)
+          #append our choice to the Rt_trend
           Rt_trend <- c(state_trend$Rt_trend, res)
           names(Rt_trend)[rt_index] <- paste0("Rt_", rt_index - 1)
           list(
-            #get the initial state
+            #get the new initial state
             initial_state = get_initial_state(res, rt_index, state_trend$initial_state),
             Rt_trend = Rt_trend
           )
+          #and repeat process
         },
         .init = list(initial_state = initial_state, Rt_trend = Rt_trend)
       )$Rt_trend
 
-      #calculate final model output
+      #calculate final model output for the whole time period covered by the data
       model_output <- model(rt_trend, t_start = 0,
                             t_end = utils::tail(utils::tail(split_data, 1)[[1]]$t_end, 1),
                             initial_state = assign_infections(setup_parameters(squire_model, parameters), initial_infections),
-                            tt_Rt = rt_df$rt_change_t)
+                            tt_Rt = rt_df$rt_change_t,
+                            #run with higher tolerance, the odin model should never fail in the fitting though it can here
+                            atol = 10^-8, rtol = 10^-8)
 
       #diagnostics are null for now
       diagnostics <- NULL
 
-      #update progressr
+      #update progressr so it knows that this sample is finished
       p()
 
       #output values
       list(
+        #our fitted Rt trend
         rt_trend = rt_trend,
+        #the times relative to the start date that Rt changes
         rt_change_t = rt_df$rt_change_t,
+        #our fitted initial number of infections
         initial_infections = initial_infections,
         model_output = model_output,
         diagnostics = diagnostics
@@ -200,6 +221,7 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
     data, initial_r, initial_infections, proposal_width, n_particles, k, squire_model
   )
   #return model object
+  #get the model itself with basic parameters with a catch for different formats this takes
   if(class(squire_model$odin_model) == "function"){
     odin_model <- squire_model$odin_model(user = setup_parameters(squire_model, parameters),
                                         unused_user_action = "ignore")
@@ -208,9 +230,9 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
                                         unused_user_action = "ignore")
   }
   return_obj <- list(
-    parameters = parameters,
+    parameters = parameters, #parameters fixed across each sample
     model = odin_model, #redundancy to keep compatible with squire/nimue functions
-    squire_model = squire_model,
+    squire_model = squire_model, #model object it self, kept for access to parameter function
     samples = purrr::map(seq_along(distribution), ~append( #add fitted Rt trends to distribution
       distribution[[.x]], list(
         R0 = particle_output[[.x]]$rt_trend,
@@ -233,9 +255,11 @@ rt_optimise <- function(data, distribution, squire_model, parameters,
     inputs = list(
       #start_date and diagnostics
       start_date = start_date,
-      data = data
+      data = data,
+      k = k
     )
   )
+  #add a tag if vaccine model
   if(any(stringr::str_detect(class(squire_model), "nimue"))){
     class(return_obj) <- c("rt_optimised", "nimue_simulation")
   } else {
--- R/particle_fit_internal.R
+++ R/particle_fit_internal.R
@@ -30,23 +30,25 @@ assert_distribution <- function(distribution){
 #' squire:::beta_est that works with the model or a method for squire.page::beta_est.
 #' @noRd
 generate_model_function <- function(squire_model, parameters){
+  #get parameters for the model, essentially fills out missing with defaults
   squire_parameters <- setup_parameters(squire_model, parameters)
   #define a function that gets the deaths for a requested period
-  function(Rt, t_start, t_end, initial_state = NULL, tt_Rt = NULL){
+  function(Rt, t_start, t_end, initial_state = NULL, tt_Rt = NULL, atol= 10^-6, rtol = 10^-6){
+    #ensures the environment contains these objects
     force(squire_parameters)
     force(squire_model)
-    #setup the initial state for the odin model
+    #set the initial state for the odin model
     if (!is.null(initial_state)) {
       squire_parameters <- initial_state
     }
-    #calculate beta value for Rt
+    #calculate beta value from Rt
     squire_parameters$beta_set <-
       beta_est(squire_model, squire_parameters, Rt)
     if(!is.null(tt_Rt)){
       squire_parameters$tt_beta <- tt_Rt
     }
     #create odin model with these parameters
-    #catch if nimue type
+    #with catch if nimue type
     if(class(squire_model$odin_model) == "function"){
       odin_model <- squire_model$odin_model(
         user = squire_parameters,
@@ -58,8 +60,8 @@ generate_model_function <- function(squire_model, parameters){
         unused_user_action = "ignore"
       )
     }
-    #run the model over the requested timeperiod
-    model_output <- odin_model$run(seq(t_start, t_end, by = 1))
+    #run the model over the requested time-period
+    model_output <- odin_model$run(seq(t_start, t_end, by = 1), atol = atol, rtol = rtol)
   }
 }
 #' Function factory for setting up the death generating function for the model
@@ -67,15 +69,18 @@ generate_model_function <- function(squire_model, parameters){
 #' squire:::beta_est that works with the model or a method for squire.page::beta_est.
 #' @noRd
 generate_deaths_function <- function(model_func){
-  #determine with columns relate to deaths
+  #determine with columns related to deaths
   temp_rn <- model_func(1, 0, 2, NULL)
+  #save there column indexes
   death_cols <- stringr::str_detect(colnames(temp_rn), "D\\[")
   rm(temp_rn)
   #define a function that gets the deaths for a requested period
   get_deaths <- function(Rt, t_start, t_end, initial_state = NULL){
     force(model_func)
     force(death_cols)
+    #generate output
     model_output <- model_func(Rt, t_start, t_end, initial_state)
+    #reduce to death compartments and reduce
     model_output[, death_cols] %>%
       rowSums()
   }
@@ -84,9 +89,8 @@ generate_deaths_function <- function(model_func){
 #' Spread initial infections out over a model
 #' @noRd
 assign_infections <- function(initial_state, initial_infections){
-  #update E1_0, might need to be model specific!
-  #spread evenly across adult population
-  #this should work fie
+  #Adds infections uniformly across adult population in the E1 compartment
+  #this should work regardless of type due to how R handles [] calls to matrices
   initial_state$E1_0[4:14] <- initial_infections * initial_state$population[4:14]/sum(initial_state$population[4:14])
   initial_state
 }
@@ -94,10 +98,10 @@ assign_infections <- function(initial_state, initial_infections){
 #' Update initial state with model output
 #' @noRd
 update_initial_state <- function(initial_state, model_output){
-  model_output <- utils::tail(model_output, 1)
-  #get_values to update
+  #get_values to update, they will have _0 in their name but we remove this to get the final values
   pars <- stringr::str_replace(names(initial_state)[stringr::str_detect(names(initial_state), "_0")], "_0", "")
   names(pars) <- pars
+  #get each value from the output
   new_values <- purrr::map(
     pars,
     function(par) {
@@ -106,7 +110,7 @@ update_initial_state <- function(initial_state, model_output){
     }
   )
   if("N_vaccine" %in% names(initial_state)){
-    #convert to matrices
+    #convert to matrices if vaccine model
     new_values <- purrr::map(
       new_values, ~matrix(.x, nrow = initial_state$N_age, ncol = initial_state$N_vaccine)
     )
@@ -121,36 +125,43 @@ update_initial_state <- function(initial_state, model_output){
 #' For now we will set it to be the shortest possible time
 #' @noRd
 get_delay <- function(squire_model, parameters){
+  #convert parameters into those fed into odin model
   pars <- setup_parameters(squire_model, parameters)
+  #get the range of rate from infection to death (if dieing)
   vals <- (2/pars$gamma_ICase) + (2/pars$gamma_E) + (2/unlist(pars[c("gamma_get_mv_die", "gamma_get_ox_die",
                                                       "gamma_not_get_mv_die", "gamma_not_get_ox_die")]))
-  list(min = floor(min(vals)), max = ceiling(max(vals)))#max(vals)))
+  #return the minimum and the maximum
+  list(min = floor(min(vals)), max = ceiling(max(vals)))
 }
 #' Get the time series of Rt trend changes and split data up
 #' @noRd
 get_time_series <- function(squire_model, parameters, data, rt_spacing){
   # Set up delay parameter
   rt_death_delay <- get_delay(squire_model, parameters)
-  #first t occures before first death so we ignore 0s
   #calculate when rt should change, so that it aligns with the data
   rt_change_t <- seq(data$t_start[data$deaths > 0][1], utils::tail(data$t_end, 1), by = rt_spacing) - rt_death_delay$min
   #remove any trends that occures on or before 0, we add R0 later anyway
   rt_change_t <- rt_change_t[rt_change_t > 0]
-  #ensure no changes estiamted in last max delay period
+  #ensure no changes estimated in last max delay period, (i.e. don't estimate an Rt in the last ~20 days)
   to_remove <- which(rt_change_t > utils::tail(data$t_end, 1) - rt_death_delay$max)
   if(length(to_remove) > 0){
     rt_change_t <- c(0, rt_change_t[-to_remove])
   } else {
     rt_change_t <- c(0, rt_change_t)
   }
-
+  #setup data frame
   rt_df <- data.frame(rt_change_t = rt_change_t)
+  #add parameter for when the next trend is started (used for simulating up to the next trend)
   rt_df$initial_target <- c(rt_change_t[-1], utils::tail(data$t_end, 1))
+  #parameter for when the fitting period starts
   rt_df$death_start_t <- rt_df$rt_change_t + rt_death_delay$min
   rt_df$death_start_t[1] <- 0
+  #parameter for when the fitting period ends
   rt_df$death_end_t <- rt_df$initial_target + rt_death_delay$max
   rt_df$death_end_t[length(rt_df$death_end_t)] <- utils::tail(data$t_end, 1)
-
+  #for convience we also split the data up into seperate data frames based on
+  #which rt_trend they relate too, note these groups likely overlap and that is
+  #intentional
   split_data <- purrr::map(seq_len(nrow(rt_df)), function(x){
     data %>%
       dplyr::filter(
@@ -163,35 +174,42 @@ get_time_series <- function(squire_model, parameters, data, rt_spacing){
         index_end = .data$t_end - rt_df$rt_change_t[x] + 1
       )
   })
-  #Some kind of check on the data?
-
+  #potentially add a check in case Rt trends have no data to fit too
 
   list(rt_df = rt_df, split_data = split_data)
 }
 #' Find the particle that maximises the likelihood for R0 and initial conditions
 #' @noRd
-particle_optimise_R0 <- function(initial_r, initial_infections, n_particles, proposal_width, R0_likelihood){
+particle_optimise_R0 <- function(initial_r, initial_infections, n_particles, proposal_width, R0_likelihood, rt_upper_bound_max = 20, rt_upper_bound_min = 5){
+  #this is identical to particle_optimise_Rt except it also explores initial infections
   repeat_limit <- 5
   repeats <- 0
   searching <- TRUE
   while(searching){
     values <- data.frame(
-      R0 = get_Rt_to_explore(initial_r, proposal_width, n_particles),
+      #get the range of Rt values
+      R0 = get_Rt_to_explore(initial_r, proposal_width, n_particles, rt_upper_bound_max, rt_upper_bound_min),
+      #get the range of initial infections to explore
       initial_infections = seq(initial_infections * (1-proposal_width), initial_infections/(1-proposal_width), length.out = n_particles)
     ) %>%
+      #get all possible compations
       tidyr::expand(.data$R0, .data$initial_infections) %>%
       dplyr::rowwise() %>%
       dplyr::mutate(
-        ll = R0_likelihood(.data$R0, .data$initial_infections)
+        #calculate their likelihood, with catch if it fails
+        ll = tryCatch(R0_likelihood(.data$R0, .data$initial_infections),
+                 error = function(e){-Inf} #negative infinity so it is not picked
+        )
       ) %>%
       dplyr::ungroup()
     #get the maximum and return R0 and initial_infections
     mle <- values %>%
       dplyr::filter(.data$ll == max(.data$ll))
     if(nrow(mle) > 1){
-      #catch if we have equal likelihoods
+      #catch if we have equal likelihoods (shouldn't happen, unless data is 0)
       mle <- mle[round(nrow(mle)/2),]
     }
+    #check if we need to repeat this (if R0 or initial_infections are on boundaries)
     update_R0 <- mle$R0 %in% c(min(values$R0), max(values$R0))
     update_initial_infections <-  mle$initial_infections %in%
       c(min(values$initial_infections), max(values$initial_infections))
@@ -214,15 +232,26 @@ particle_optimise_R0 <- function(initial_r, initial_infections, n_particles, pro
 }
 #' Find the particle that optimises the likelihood for Rt
 #' @noRd
-particle_optimise_Rt <- function(rt, initial_state, rt_index, n_particles, proposal_width, likelihood){
+particle_optimise_Rt <- function(rt, initial_state, rt_index, n_particles, proposal_width, likelihood, rt_upper_bound_max = 20, rt_upper_bound_min = 5){
+  #we allow this search to repeat a maximum of 5 times, if the maximum likelihood
+  #is on the boundary of the searched values, then we repeat with it recentered on
+  #that value
   repeat_limit <- 5
   repeats <- 0
   searching <- TRUE
   while(searching){
-    rt_to_explore <- get_Rt_to_explore(rt, proposal_width, n_particles)
-    ll <- purrr::map_dbl(rt_to_explore, ~likelihood(.x, rt_index, initial_state))
+    #get values to explore
+    rt_to_explore <- get_Rt_to_explore(rt, proposal_width, n_particles, rt_upper_bound_max, rt_upper_bound_min)
+    #calculate R0
+    ll <- purrr::map_dbl(rt_to_explore, function(this_rt){
+      #add a catch if this fails
+      tryCatch(likelihood(this_rt, rt_index, initial_state),
+               error = function(e){-Inf} #rerun negative infinity so it is not picked
+      )
+    })
+    #pick the highest
     best <- rt_to_explore[which.max(ll)]
-
+    #check if on a boundary
     update_Rt <- best %in% c(min(rt_to_explore), max(rt_to_explore))
 
     if(update_Rt){
@@ -239,11 +268,32 @@ particle_optimise_Rt <- function(rt, initial_state, rt_index, n_particles, propo
 }
 #' Get the rt values to explore
 #' @noRd
-get_Rt_to_explore <- function(rt, proposal_width, n_particles){
-  rt_max <- 20
+get_Rt_to_explore <- function(rt, proposal_width, n_particles, rt_upper_bound_max = 20, rt_upper_bound_min = 5){
+  #set bounds on maximums to avoid ridiculous values or being stuck below 1
   upper <- rt/(1-proposal_width)
-  if(upper > rt_max){
-    upper <- rt_max
+  if(upper > rt_upper_bound_max){
+    upper <- rt_upper_bound_max
+  } else if(upper < rt_upper_bound_min){
+    upper <- rt_upper_bound_min
   }
+  #generate values
   seq(rt * (1-proposal_width), upper, length.out = n_particles)
 }
+#' Function to calculate ll with the negative binomial
+#'
+#' Separate function so it can be used in trimming too
+#'
+#' @noRd
+ll_negative_binomial <- function(model_deaths, data_deaths, k){
+  model_deaths <- dplyr::if_else(model_deaths < 0, 10^-10, model_deaths)
+  #calculate likelihood for each time-period then sum
+  squire:::ll_nbinom(
+    data = data_deaths,
+    model = model_deaths,
+    phi = 1,
+    k = k,
+    exp_noise = Inf #no noise
+  ) %>%
+    sum()
+}
+
